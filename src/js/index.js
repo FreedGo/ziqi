d7a51819ebb71c5f78f6195a4115b167cfc2d2b0
Freed <flyxz@126.com>
2017-08-29 18:47:29 +0800

    顶部通知条未调好

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,15 @@
  * E-mail:flyxz@126.com.
  * GitHub:FreedGo@github.com.
  */
-console.log(1)
+$(function () {
+	//1.顶部信息轮播
+	$(".fullSlide").slide({
+		titCell:".hd ul",
+		mainCell:".bd ul",
+		effect:"leftMarquee",
+		autoPlay:true,
+		trigger:"click",
+		interTime:1000,
+		delayTime:3000
+	});
+})
